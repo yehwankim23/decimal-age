74b363291760172cbce134627d9bbd2cfbfba321
yehwankim23 <yehwankim23@gmail.com>
2022-02-07 01:33:57 +0900

    Add calculator

--- script.js
+++ script.js
@@ -17,3 +17,65 @@ const container = document.getElementById("container");
 container.setAttribute("style", `width: ${width}px; height: ${height}px;`);
 container.width = width;
 container.height = height;
+
+const birthday = document.getElementById("birthday");
+const age = document.getElementById("age");
+
+function getTotalDays(year) {
+  return year % 400 == 0 ? 366 : year % 100 == 0 ? 365 : year % 4 == 0 ? 366 : 365;
+}
+
+function getDayNumber(year, month, day) {
+  switch (month) {
+    case 12:
+      day += 30;
+    case 11:
+      day += 31;
+    case 10:
+      day += 30;
+    case 9:
+      day += 31;
+    case 8:
+      day += 31;
+    case 7:
+      day += 30;
+    case 6:
+      day += 31;
+    case 5:
+      day += 30;
+    case 4:
+      day += 31;
+    case 3:
+      day += getTotalDays(year) == 365 ? 28 : 29;
+    case 2:
+      day += 31;
+  }
+
+  return day;
+}
+
+function calculate() {
+  let input = birthday.value;
+  let birthdayYear = parseInt(input.slice(0, 4));
+
+  let today = new Date();
+  today = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() +
+      getTotalDays(birthdayYear) -
+      getDayNumber(birthdayYear, parseInt(input.slice(5, 7)), parseInt(input.slice(8, 10)))
+  );
+  let todayYear = today.getFullYear();
+
+  age.innerText = `You are ${
+    todayYear -
+    birthdayYear -
+    1 +
+    (getDayNumber(todayYear, today.getMonth() + 1, today.getDate()) / getTotalDays(todayYear))
+      .toString()
+      .slice(1, 3)
+  } years old.`;
+}
+
+calculate();
